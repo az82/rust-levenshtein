74f0ed3336000fcbf24bf76bb3fb58cbdb40417a
Andreas Zitzelsberger <az@az82.de>
2019-01-18 14:56:47 +0100

    unit test added

--- src/lfilter.rs
+++ src/lfilter.rs
@@ -23,6 +23,7 @@ pub struct FilteredWords<'a> {
     max_distance: usize,
 }
 
+
 impl<'a> Iterator for FilteredWords<'a> {
     type Item = io::Result<String>;
 
@@ -31,12 +32,81 @@ impl<'a> Iterator for FilteredWords<'a> {
             match self.words.next() {
                 Some(r) => match r {
                     Ok(w) => if levenshtein(self.search_word, &w) <= self.max_distance {
-                        return Some(Ok(w));
+                        return Some(Ok(w))
                     },
                     Err(e) => return Some(Err(e))
                 },
                 None => return None
             }
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filter_words_one() {
+        // Given
+        let words = vec![
+            Ok("tree".to_string()),
+            Ok("flower".to_string()),
+            Ok("mouse".to_string())];
+        let words_iter = &mut words.into_iter();
+
+        // When
+        let filtered_words = &mut filter_words(words_iter, "house", 1);
+
+        // Then
+        match filtered_words.next() {
+            Some(r) => match r {
+                Ok(w) => assert_eq!(w, "mouse"),
+                Err(_) => panic!("assertion failed: first item should be Ok(_), but was Err(_)")
+            }
+            None => panic!("assertion failed: first item should be Some(_))")
+        }
+    }
+
+    #[test]
+    fn filter_words_none() {
+        // Given
+        let words = vec![
+            Ok("tree".to_string()),
+            Ok("flower".to_string()),
+            Ok("stone".to_string())];
+        let words_iter = &mut words.into_iter();
+
+        // When
+        let filtered_words = &mut filter_words(words_iter, "house", 1);
+
+        // Then
+        match filtered_words.next() {
+            Some(_) => panic!("assertion failed: first item should be None() but was Some(_)"),
+            None => {}
+        }
+    }
+
+    #[test]
+    fn filter_words_err() {
+        // Given
+        let words = vec![
+            Ok("tree".to_string()),
+            Ok("flower".to_string()),
+            Err(io::Error::new(io::ErrorKind::Other, ""))];
+        let words_iter = &mut words.into_iter();
+
+        // When
+        let filtered_words = &mut filter_words(words_iter, "house", 1);
+
+        // Then
+        match filtered_words.next() {
+            Some(r) => match r {
+                Ok(_) => panic!("assertion failed: first item should be ERR(_))"),
+                Err(_) => {}
+            }
+            None => panic!("assertion failed: first item should be Some(_))")
         }
     }
 }
